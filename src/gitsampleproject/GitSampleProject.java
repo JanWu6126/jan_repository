780fef552c91b5354b8b33ff4d9d9ea32579bba7
BA201 <BA201@MCBA201-C04.classrooms.stlcc>
2016-10-17 18:41:00 -0500

    Add sayHello() method Description: print hello

--- src/gitsampleproject/GitSampleProject.java
+++ src/gitsampleproject/GitSampleProject.java
@@ -15,7 +15,10 @@ public class GitSampleProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // sayHello();
+        sayHello();
     }
-    
+    private static void sayHello() {
+        System.out.println("Hello World!");
+    }
 }
